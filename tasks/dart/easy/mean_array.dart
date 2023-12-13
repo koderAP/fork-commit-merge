5043ebab2903d3c0254442221d411fe40b1b5721
ANUBHAV PANDEY <113716152+koderAP@users.noreply.github.com>
2023-12-13 09:10:12 +0530

    Update mean_array.dart
    solves #1589

--- tasks/dart/easy/mean_array.dart
+++ tasks/dart/easy/mean_array.dart
@@ -1,8 +1,20 @@
-void main() {
-  // List<int> data = [5, 10, 15, 20, 25];
-  // double mean = calculateMean(data);
-  // print('Mean: $mean');  // Expected output: Mean: 15.0
+double calculateMean(List<int> numbers) {
+  if (numbers.isEmpty) {
+    //division by zero
+    throw ArgumentError("Input list must not be empty, 0/0 not defined");
+  }
+
+  int sum = 0;
+  for (int number in numbers) {
+    sum += number;
+  }
+
+  double mean = sum / numbers.length;
+  return mean;
 }
 
-// TODO: Implement a function called 'calculateMean' that takes a list of integers as input and returns the mean as a double.
-
+void main() {
+  List<int> numberList = [1, 2, 3, 4, 5];
+  double mean = calculateMean(numberList);
+  print('Mean: $mean');
+}
